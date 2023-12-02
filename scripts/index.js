f1b5e32278cf64db11510b1dea17f5dc63b33583
Robert Bedea <robertbedea@gmail.com>
2023-12-02 12:13:12 -0600

    Identifier error resolved.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-cons intialcards = [
+const intialcards = [
     {
         title: "Yosemite Valley",
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
