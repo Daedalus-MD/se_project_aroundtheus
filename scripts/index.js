fd1bb7c75e31cf7609058a1537a76e98d9d5ed11
Robert Bedea <robertbedea@gmail.com>
2023-12-02 14:04:54 -0600

    Card data.

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,32 @@
-const intialcards = [
-  {
+
+  const carddata1= {
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  },
+  };
   {
     title: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  },
+  }
+  const carddata2=
   {
     title: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
+  const carddata3=
   {
     title: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
-  {
+  
+  const carddata4=  {
     title: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
+  const carddata5=
   {
     title: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-  },
-];
-
+  };
+  
+const intialcards = [cardData]
 console.log(InitialCards);
