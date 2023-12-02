f2310c1e7cc2ca6eb07d6c5b238dd27d75445ca6
Robert Bedea <robertbedea@gmail.com>
2023-12-02 12:15:50 -0600

    Comma seperation added.

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,23 @@ const intialcards = [
     {
         title: "Yosemite Valley",
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-    }
+    },
     {
         title: "Lake Louise",
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-    }
+    },
     {
         title: "Bald Mountains",
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-    }
+    },
     {
         title: "Latemar",
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-    }
+    },
     {    
         title: "Vanoise National Park"
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-    }   
+    },
     {    
         title: "Lago di Braies"
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
