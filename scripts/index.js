83352dba34a9bce263cacb9fbfff6eadf3bad609
Robert Bedea <robertbedea@gmail.com>
2023-12-02 14:07:17 -0600

    Remade code.

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,27 @@
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   };
+  const carddata2=
   {
     title: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  }
-  const carddata2=
+  };
+  const carddata3=
   {
     title: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
-  const carddata3=
+  const carddata4=
   {
     title: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
   
-  const carddata4=  {
+  const carddata5=  {
     title: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
-  const carddata5=
+  const carddata6=
   {
     title: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
